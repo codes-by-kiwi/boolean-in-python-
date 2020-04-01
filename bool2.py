5ffc625840bead0dbb6f0f33be11bebc28faca4f
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 14:05:15 +0200

    Add files via upload

---
+++ bool2.py
@@ -0,0 +1,7 @@
+student = "Oliver"
+age = 15
+if student == "Oliver" and age == 15: #this is an and statement. if any of these are true the next line will be executed. 
+    print("You have arrived to the right classroom ")
+
+if student == "Linda" or student == "Mapel": #this is an or statement. if any of these conditions are true the next line will be executed. 
+    print("Ms.Cassidy is looking for you") 
