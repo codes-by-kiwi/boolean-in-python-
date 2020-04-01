b8ee5caccee4caa2e7c7fc851dd975a812af1c12
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 14:01:14 +0200

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,5 @@
+x = 10
+print(x == 2) # prints out false since 10 is not equal to 2 
+print(x == 3) # prints out False since 10 is not equal to 3
+print(x < 3) # prints out false since 10 is not less than 3 
+print(x>3) # prints out true since 10 is greater than 3 
